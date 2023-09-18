a05cfbc18b3f41cf48630fcc61e3f6726a2b451a
Po-Yen, Chen <PoYen.Chen@amd.com>
2023-09-18 14:16:53 -0400

    More definition of Traits types

--- include/ck/tile_program/tile/tile_window_impl_static_distribution.hpp
+++ include/ck/tile_program/tile/tile_window_impl_static_distribution.hpp
@@ -96,122 +96,6 @@ struct TileWindowWithStaticDistribution
     using BottomTensorCoord =
         decltype(make_tensor_coordinate(BottomTensorDesc{}, BottomTensorIndex{}));
 
-    __device__ constexpr TileWindowWithStaticDistribution() = default;
-
-    __device__ constexpr TileWindowWithStaticDistribution(
-        const BottomTensorView& bottom_tensor_view,
-        const WindowLengths& window_lengths,
-        const BottomTensorIndex& window_origin,
-        const TileDstr& tile_distribution)
-        : bottom_tensor_view_{bottom_tensor_view},
-          window_lengths_{window_lengths},
-          window_origin_{window_origin},
-          bottom_tensor_thread_coord_{},
-          tile_dstr_{tile_distribution},
-          window_adaptor_thread_coord_{
-              make_tensor_adaptor_coordinate(tile_distribution.GetPsYs2XsAdaptor(),
-                                             AdaptorTopIndex{get_warp_id(), get_lane_id(), 0})}
-    {
-        BottomTensorIndex bottom_tensor_thread_origin_idx;
-
-        for(index_t i = 0; i < NDimBottomTensor; ++i)
-        {
-            bottom_tensor_thread_origin_idx(i) =
-                window_origin[i] + window_adaptor_thread_coord_.GetBottomIndex()[i];
-        }
-
-        bottom_tensor_thread_coord_ = make_tensor_coordinate(
-            bottom_tensor_view_.GetTensorDescriptor(), bottom_tensor_thread_origin_idx);
-    }
-
-    __device__ static constexpr index_t GetNumOfDimension() { return NDimBottomTensor; }
-
-    __device__ static constexpr bool HasStaticTileDistribution() { return TileDstr::IsStatic(); }
-
-    __device__ constexpr auto GetWindowLengths() const { return window_lengths_; }
-
-    __device__ constexpr auto GetTileDistribution() const { return tile_dstr_; }
-
-    __device__ constexpr auto GetBottomTensorView() const { return bottom_tensor_view_; }
-
-    __device__ constexpr auto GetWindowOrigin() const { return window_origin_; }
-
-    __device__ constexpr auto GetBottomTensorThreadCoordinate() const
-    {
-        return bottom_tensor_thread_coord_;
-    }
-
-    // move thread's window adaptor coordiante
-    // [p0, p1, ..., y0, y1, ...] ==> [x0, x1, ...]
-    __device__ void MoveWindowAdaptorThreadCoordinate(const AdaptorTopIndex& idx_diff_adaptor)
-    {
-        move_tensor_adaptor_coordinate(
-            tile_dstr_.GetPsYs2XsAdaptor(), window_adaptor_thread_coord_, idx_diff_adaptor);
-    }
-
-    // move thread's botom tensor coordiante
-    // [x0', x1', ... ] ==> [offset]
-    __device__ void MoveBottomTensorThreadCoordinate(const BottomTensorIndex& idx_diff_tensor)
-    {
-        move_tensor_coordinate(bottom_tensor_view_.GetTensorDescriptor(),
-                               bottom_tensor_thread_coord_,
-                               idx_diff_tensor);
-    }
-
-    // move thread's window adaptor coordinate and bottom tensor coordinate
-    // [p0, p1, ..., y0, y1, ...] ==> [x0, x1, ...] ==> [x0', x1', ...] ==> [offset]
-    __device__ void
-    MoveWindowAdaptorAndBottomTensorThreadCoordinate(const AdaptorTopIndex& idx_diff_adaptor_top)
-    {
-        Array<index_t, NDimBottomTensor> idx_diff_adaptor_bottom;
-
-        move_tensor_adaptor_coordinate(tile_dstr_.GetPsYs2XsAdaptor(),
-                                       window_adaptor_thread_coord_,
-                                       idx_diff_adaptor_top,
-                                       idx_diff_adaptor_bottom);
-
-        move_tensor_coordinate(bottom_tensor_view_.GetTensorDescriptor(),
-                               bottom_tensor_thread_coord_,
-                               idx_diff_adaptor_bottom);
-    }
-
-    // return vector dimension among [y0, y1, ...]
-    __device__ static constexpr auto GetWindowAdaptorYsSafeVectorLengthStrides()
-    {
-        // bottom tensor top dimension vector lengths and strides
-        const auto [bottom_tensor_top_dim_vector_lengths, bottom_tensor_top_dim_vector_strides] =
-            BottomTensorDesc::GetTopDimensionSafeVectorLengthStrides();
-
-        // window vector lengths/strides
-        const auto window_adaptor_bottom_dim_vector_lengths = bottom_tensor_top_dim_vector_lengths;
-        const auto window_adaptor_bottom_dim_vector_strides = bottom_tensor_top_dim_vector_strides;
-
-        // window adaptor [p0, p1, ..., y0, y1, ...]
-        Array<index_t, WindowAdaptor::GetNumOfHiddenDimension()> window_adaptor_vector_lengths{-1};
-        Array<index_t, WindowAdaptor::GetNumOfHiddenDimension()> window_adaptor_vector_strides{-1};
-
-        constexpr auto window_adaptor_bottom_dims = WindowAdaptor::GetBottomDimensionHiddenIds();
-
-        set_container_subset(window_adaptor_vector_lengths,
-                             window_adaptor_bottom_dims,
-                             window_adaptor_bottom_dim_vector_lengths);
-        set_container_subset(window_adaptor_vector_strides,
-                             window_adaptor_bottom_dims,
-                             window_adaptor_bottom_dim_vector_strides);
-
-        const auto [window_adaptor_ps_ys_vector_lengths, window_adaptor_ps_ys_vector_strides] =
-            WindowAdaptor{}.GetTopDimensionSafeVectorLengthStrides(window_adaptor_vector_lengths,
-                                                                   window_adaptor_vector_strides);
-
-        // [y0, y1, ...]
-        constexpr auto y_dims = typename arithmetic_sequence_gen<TileDstr::GetNumOfDimensionP(),
-                                                                 NDimWindowAdaptorTop,
-                                                                 1>::type{};
-
-        return make_tuple(get_container_subset(window_adaptor_ps_ys_vector_lengths, y_dims),
-                          get_container_subset(window_adaptor_ps_ys_vector_strides, y_dims));
-    }
-
     struct TraitsBase
     {
         protected:
@@ -357,6 +241,122 @@ struct TileWindowWithStaticDistribution
         StoreTraits::NumAccess; /// TODO: enlarge this number if unnecessary
     static constexpr index_t NumAccessForStore = StoreTraits::NumAccess;
     static constexpr index_t MaxNumAccess      = math::max(NumAccessForLoad, NumAccessForStore);
+
+    __device__ constexpr TileWindowWithStaticDistribution() = default;
+
+    __device__ constexpr TileWindowWithStaticDistribution(
+        const BottomTensorView& bottom_tensor_view,
+        const WindowLengths& window_lengths,
+        const BottomTensorIndex& window_origin,
+        const TileDstr& tile_distribution)
+        : bottom_tensor_view_{bottom_tensor_view},
+          window_lengths_{window_lengths},
+          window_origin_{window_origin},
+          bottom_tensor_thread_coord_{},
+          tile_dstr_{tile_distribution},
+          window_adaptor_thread_coord_{
+              make_tensor_adaptor_coordinate(tile_distribution.GetPsYs2XsAdaptor(),
+                                             AdaptorTopIndex{get_warp_id(), get_lane_id(), 0})}
+    {
+        BottomTensorIndex bottom_tensor_thread_origin_idx;
+
+        for(index_t i = 0; i < NDimBottomTensor; ++i)
+        {
+            bottom_tensor_thread_origin_idx(i) =
+                window_origin[i] + window_adaptor_thread_coord_.GetBottomIndex()[i];
+        }
+
+        bottom_tensor_thread_coord_ = make_tensor_coordinate(
+            bottom_tensor_view_.GetTensorDescriptor(), bottom_tensor_thread_origin_idx);
+    }
+
+    __device__ static constexpr index_t GetNumOfDimension() { return NDimBottomTensor; }
+
+    __device__ static constexpr bool HasStaticTileDistribution() { return TileDstr::IsStatic(); }
+
+    __device__ constexpr auto GetWindowLengths() const { return window_lengths_; }
+
+    __device__ constexpr auto GetTileDistribution() const { return tile_dstr_; }
+
+    __device__ constexpr auto GetBottomTensorView() const { return bottom_tensor_view_; }
+
+    __device__ constexpr auto GetWindowOrigin() const { return window_origin_; }
+
+    __device__ constexpr auto GetBottomTensorThreadCoordinate() const
+    {
+        return bottom_tensor_thread_coord_;
+    }
+
+    // move thread's window adaptor coordiante
+    // [p0, p1, ..., y0, y1, ...] ==> [x0, x1, ...]
+    __device__ void MoveWindowAdaptorThreadCoordinate(const AdaptorTopIndex& idx_diff_adaptor)
+    {
+        move_tensor_adaptor_coordinate(
+            tile_dstr_.GetPsYs2XsAdaptor(), window_adaptor_thread_coord_, idx_diff_adaptor);
+    }
+
+    // move thread's botom tensor coordiante
+    // [x0', x1', ... ] ==> [offset]
+    __device__ void MoveBottomTensorThreadCoordinate(const BottomTensorIndex& idx_diff_tensor)
+    {
+        move_tensor_coordinate(bottom_tensor_view_.GetTensorDescriptor(),
+                               bottom_tensor_thread_coord_,
+                               idx_diff_tensor);
+    }
+
+    // move thread's window adaptor coordinate and bottom tensor coordinate
+    // [p0, p1, ..., y0, y1, ...] ==> [x0, x1, ...] ==> [x0', x1', ...] ==> [offset]
+    __device__ void
+    MoveWindowAdaptorAndBottomTensorThreadCoordinate(const AdaptorTopIndex& idx_diff_adaptor_top)
+    {
+        Array<index_t, NDimBottomTensor> idx_diff_adaptor_bottom;
+
+        move_tensor_adaptor_coordinate(tile_dstr_.GetPsYs2XsAdaptor(),
+                                       window_adaptor_thread_coord_,
+                                       idx_diff_adaptor_top,
+                                       idx_diff_adaptor_bottom);
+
+        move_tensor_coordinate(bottom_tensor_view_.GetTensorDescriptor(),
+                               bottom_tensor_thread_coord_,
+                               idx_diff_adaptor_bottom);
+    }
+
+    // return vector dimension among [y0, y1, ...]
+    __device__ static constexpr auto GetWindowAdaptorYsSafeVectorLengthStrides()
+    {
+        // bottom tensor top dimension vector lengths and strides
+        const auto [bottom_tensor_top_dim_vector_lengths, bottom_tensor_top_dim_vector_strides] =
+            BottomTensorDesc::GetTopDimensionSafeVectorLengthStrides();
+
+        // window vector lengths/strides
+        const auto window_adaptor_bottom_dim_vector_lengths = bottom_tensor_top_dim_vector_lengths;
+        const auto window_adaptor_bottom_dim_vector_strides = bottom_tensor_top_dim_vector_strides;
+
+        // window adaptor [p0, p1, ..., y0, y1, ...]
+        Array<index_t, WindowAdaptor::GetNumOfHiddenDimension()> window_adaptor_vector_lengths{-1};
+        Array<index_t, WindowAdaptor::GetNumOfHiddenDimension()> window_adaptor_vector_strides{-1};
+
+        constexpr auto window_adaptor_bottom_dims = WindowAdaptor::GetBottomDimensionHiddenIds();
+
+        set_container_subset(window_adaptor_vector_lengths,
+                             window_adaptor_bottom_dims,
+                             window_adaptor_bottom_dim_vector_lengths);
+        set_container_subset(window_adaptor_vector_strides,
+                             window_adaptor_bottom_dims,
+                             window_adaptor_bottom_dim_vector_strides);
+
+        const auto [window_adaptor_ps_ys_vector_lengths, window_adaptor_ps_ys_vector_strides] =
+            WindowAdaptor{}.GetTopDimensionSafeVectorLengthStrides(window_adaptor_vector_lengths,
+                                                                   window_adaptor_vector_strides);
+
+        // [y0, y1, ...]
+        constexpr auto y_dims = typename arithmetic_sequence_gen<TileDstr::GetNumOfDimensionP(),
+                                                                 NDimWindowAdaptorTop,
+                                                                 1>::type{};
+
+        return make_tuple(get_container_subset(window_adaptor_ps_ys_vector_lengths, y_dims),
+                          get_container_subset(window_adaptor_ps_ys_vector_strides, y_dims));
+    }
 
     template <typename YIndex, index_t... YSliceLengths>
     __device__ auto LoadSlicedThreadData(const YIndex& ys_slice_origin, Sequence<YSliceLengths...>)
