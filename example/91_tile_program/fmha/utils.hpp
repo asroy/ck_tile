5b6fe07afcc1fc0cc24bc99b80c3224883526007
Po-Yen, Chen <PoYen.Chen@amd.com>
2024-01-02 10:01:10 -0500

    Fix formating

--- example/91_tile_program/fmha/utils.hpp
+++ example/91_tile_program/fmha/utils.hpp
@@ -23,25 +23,25 @@ std::ostream& operator<<(std::ostream& stream, Mode mode);
 std::vector<int32_t> to_seqstarts(ck::span<const int32_t> seqlens);
 
 std::vector<int32_t> generate_seqlens_q(Mode mode,
-                                               unsigned count,
-                                               int32_t seqlens_q_sum,
-                                               std::optional<unsigned> seed = std::nullopt);
+                                        unsigned count,
+                                        int32_t seqlens_q_sum,
+                                        std::optional<unsigned> seed = std::nullopt);
 
 std::tuple<std::vector<int32_t>, std::vector<int32_t>> generate_seqlens_seqstarts_q(
     Mode mode, unsigned count, int32_t seqlens_q_sum, std::optional<unsigned> seed = std::nullopt);
 
 std::vector<int32_t> generate_seqlens_k(Mode mode,
-                                               unsigned count,
-                                               int32_t seqlens_k_sum,
-                                               ck::span<const int32_t> seqlens_q,
-                                               int32_t seqlens_q_sum,
-                                               std::optional<unsigned> seed = std::nullopt);
+                                        unsigned count,
+                                        int32_t seqlens_k_sum,
+                                        ck::span<const int32_t> seqlens_q,
+                                        int32_t seqlens_q_sum,
+                                        std::optional<unsigned> seed = std::nullopt);
 
 std::vector<int32_t> generate_seqstarts_k(Mode mode,
-                                                 unsigned count,
-                                                 int32_t seqlens_k_sum,
-                                                 ck::span<const int32_t> seqlens_q,
-                                                 int32_t seqlens_q_sum,
-                                                 std::optional<unsigned> seed = std::nullopt);
+                                          unsigned count,
+                                          int32_t seqlens_k_sum,
+                                          ck::span<const int32_t> seqlens_q,
+                                          int32_t seqlens_q_sum,
+                                          std::optional<unsigned> seed = std::nullopt);
 
 int env_get_int(const char* var_name, int default_int);
